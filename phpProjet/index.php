9660d75a075f68f926effe38ec5607a0ad214a26
sergeabba <serge-mbaitadjim.abba@ism.edu.sn>
2020-05-31 19:59:50 +0000

    projetquizz

--- phpProjet/index.php
+++ phpProjet/index.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     
 </head>
-<?php
 <body style="">
     <style>
     body {
@@ -92,7 +91,7 @@
     
         </nav>
     </header>
-    
+   
     <div style="background-color: #fefefe;width: 484px;height: 400px;margin: 40px -2px 99px 295px;">
         <div class="d-flex" style="background-color: #51BFD0;height: 72px;">
             <p style="width: 387px;margin: 16px 18px;font-size: 24px;color: rgb(248,253,253);">
@@ -102,15 +101,14 @@
             <img src="img/ic-login.png" style="width: 30px;margin: -13px 0px 0px -45px;">
         </div>
         <div>
-            <form action="traitement.php" method="POST"></form>
             <input class="form-control-lg" type="password"  required="" placeholder="Password" pattern="Login" style="height: 59px;margin: 27px 0px 0px 25px;width: 429px;">
             <img src="img/lock-security-pad-key.png" style="width: 25px;height: 34px;margin: -17px 0px 0px -41px;">
         </div>
         <div
             class="d-lg-flex align-items-lg-center">
-            <button class="btn btn-primary" type="button" style="margin: 27px -21px 0px 24px;height: 43px;background-color: rgb(81,191,208);color: rgb(248,253,253);">
+            <button name="connexion"  type="submit" style="margin: 27px -21px 0px 24px;height: 43px;background-color: rgb(81,191,208);color: rgb(248,253,253);">
                 Connexion
-            </button><a href="user.html" style="margin: 25px 0px 0px 69px;font-size: 20px;color: rgb(129,129,129);">
+            </button><a href="user.php" style="margin: 25px 0px 0px 69px;font-size: 20px;color: rgb(129,129,129);">
                     S'inscrire pour jouer? 
                 </a></div>
 </div>
@@ -118,6 +116,6 @@
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
-}
 
-</html>    
+</html>
+   
